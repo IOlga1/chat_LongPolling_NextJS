b069a7058520233bd0a69628f9df67c1c579f013
IOlga1 <ivaniushyna.olga@gmail.com>
2023-06-25 02:26:18 +0300

    added basic pages and folders

---
+++ client/pages/404.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      XXXXXX
+    </div>
+  );
+};
+
+export default NotFoundPage;
---
+++ client/services/DataService.ts
@@ -0,0 +1,11 @@
+const sendMessage = () => {
+  // post 'http://localhost:5000/message'
+}
+
+const subscribe = () => {
+  // get 'http://localhost:5000/message'
+}
+
+export default {
+  sendMessage,
+}
